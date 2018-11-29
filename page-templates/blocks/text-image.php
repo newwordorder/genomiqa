d39c641ca65c2a94b2563ece2f3d768868acc863
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-11-29 10:15:30 +1000

    Changes made when adding a couple new pages

--- page-templates/blocks/text-image.php
+++ page-templates/blocks/text-image.php
@@ -16,14 +16,66 @@ $videoCoverImage = get_sub_field('video_cover_image');
 $layout = get_sub_field('layout');
 $flipLayout = get_sub_field('flip_layout');
 $spaceBelow = get_sub_field('space_below');
+$overlap = get_sub_field('overlap');
 
 
 ?>
+  <?php if($overlap): ?>
+    <div class="container space-below--<?php echo $spaceBelow ?>">
 
-  <div class="container space-below--<?php echo $spaceBelow ?>">
+        <div class="row flippable <?php if( $flipLayout == 'yes' ): echo 'flippable--flip'; endif; ?>">
+            <div class=" flippable__text <?php if( $flipLayout == 'yes' ): if( $layout == '1/3' ): echo 'col-md-9'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-7'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-5'; endif; else: if( $layout == '1/3' ): echo 'col-md-8'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-6'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-4'; endif; endif; ?> overlap" >
+                <?php echo $text ?>
+                <?php get_template_part( 'page-templates/blocks/block-partials/buttons' ); ?>
+            </div>
+            <div class="<?php if( $layout == '1/3' ): echo 'col-md-4'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-6'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-8'; endif; ?> flippable__image">
+
+              <?php if( $media == 'image' ): ?>
+                <?php if( !empty($image) ):
+
+                  // vars
+                  $url = $image['url'];
+                  $alt = $image['alt'];
+
+                ?>
+                  <img class="" src="<?php echo $url; ?>" alt="<?php echo $alt; ?>"/>
+                <?php endif; //end $image ?>
+
+              <?php endif; //end $media ?>
+
+              <?php if( $media == 'gallery' ): ?>
+
+              <?php endif; //end $media ?>
+
+              <?php if( $media == 'video' ): ?>
+
+                <div class="video-cover ">
+                  <div class="background-image-holder ">
+                    <?php if( !empty($videoCoverImage) ):
+
+                      // vars
+                      $url = $videoCoverImage['url'];
+                      $alt = $videoCoverImage['alt'];
+
+                    ?>
+                      <img class="" src="<?php echo $url; ?>" alt="<?php echo $alt; ?>"/>
+                    <?php endif; //end $image ?>
+                  </div>
+                  <div class="video-play-icon video-play-icon--sm"></div>
+                  <div class="embed-container ">
+                    <?php echo $videoEmbedCode; ?>
+                  </div>
+                </div><!--end video cover-->
+
+              <?php endif; //end $media ?>
+            </div>
+        </div>
+      </div>
+    <?php else: ?>
+    <div class="container space-below--<?php echo $spaceBelow ?>">
 
       <div class="row flippable <?php if( $flipLayout == 'yes' ): echo 'flippable--flip'; endif; ?>">
-          <div class=" flippable__text <?php if( $flipLayout == 'yes' ): if( $layout == '1/3' ): echo 'col-md-9'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-7'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-5'; endif; else: if( $layout == '1/3' ): echo 'col-md-8'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-6'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-4'; endif; endif; ?>" >
+          <div class=" flippable__text <?php if( $flipLayout == 'yes' ): if( $layout == '1/3' ): echo 'col-md-8'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-6'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-4'; endif; else: if( $layout == '1/3' ): echo 'col-md-8'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-6'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-4'; endif; endif; ?>" >
               <?php echo $text ?>
               <?php get_template_part( 'page-templates/blocks/block-partials/buttons' ); ?>
           </div>
@@ -36,7 +88,7 @@ $spaceBelow = get_sub_field('space_below');
                 $url = $image['url'];
                 $alt = $image['alt'];
 
-               ?>
+              ?>
                 <img class="" src="<?php echo $url; ?>" alt="<?php echo $alt; ?>"/>
               <?php endif; //end $image ?>
 
@@ -49,27 +101,29 @@ $spaceBelow = get_sub_field('space_below');
             <?php if( $media == 'video' ): ?>
 
               <div class="video-cover ">
-              	<div class="background-image-holder ">
+                <div class="background-image-holder ">
                   <?php if( !empty($videoCoverImage) ):
 
                     // vars
                     $url = $videoCoverImage['url'];
                     $alt = $videoCoverImage['alt'];
 
-                   ?>
+                  ?>
                     <img class="" src="<?php echo $url; ?>" alt="<?php echo $alt; ?>"/>
                   <?php endif; //end $image ?>
-              	</div>
-              	<div class="video-play-icon video-play-icon--sm"></div>
+                </div>
+                <div class="video-play-icon video-play-icon--sm"></div>
                 <div class="embed-container ">
-              	   <?php echo $videoEmbedCode; ?>
+                  <?php echo $videoEmbedCode; ?>
                 </div>
               </div><!--end video cover-->
 
             <?php endif; //end $media ?>
           </div>
       </div>
-    </div>
+      </div>
+
+    <?php endif; ?>
 
 <?php endif; //end text_image row
 
