5b15843842ef88d50e569678ba4a5a8f267ae73e
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-12-17 11:43:23 +1000

    Fixed Buttons and added download capacity

--- page-templates/blocks/block-partials/buttons.php
+++ page-templates/blocks/block-partials/buttons.php
@@ -4,12 +4,20 @@
   <div class="buttons">
     <?php while( have_rows('buttons') ): the_row();
       $buttonText = get_sub_field('button_text');
+      $linkType = get_sub_field('link_type');
       $url = get_sub_field('url');
+      $pageUrl = get_sub_field('page_url');
+      $file = get_sub_field('file');
       $buttonStyle = get_sub_field('button_style');
       ?>
 
-      <a href="<?php echo $url ?>" class="btn btn--<?php echo $buttonStyle ?>"><?php echo $buttonText ?> <?php if($buttonStyle == 'link'): ?><i class="fas fa-arrow-right"></i><?php endif; ?></a>
-
+      <?php if($linkType != "file"): ?>
+        <a href="<?php if($linkType == "page"): echo $pageUrl; endif; ?><?php if($linkType == "file"): echo $file; endif; ?><?php if($linkType == "url"):  echo $url; endif; ?>" class="btn btn--<?php echo $buttonStyle ?>" <?php if($linkType == "file"): echo 'download'; endif; ?>><?php if($buttonStyle == "link"): echo '<i class="far fa-arrow-circle-right"></i>'; endif; ?><?php if($buttonStyle == "outline"): echo '<span>'; endif; ?><?php echo $buttonText ?><?php if($buttonStyle == "outline"): echo '</span>'; endif; ?> </a>
+      <?php else: ?> 
+      <form method="get" action="<?php echo $file; ?>">
+        <button class="btn btn--<?php echo $buttonStyle ?>" type="submit"><?php if($buttonStyle == "link"): echo '<i class="far fa-arrow-circle-right"></i>'; endif; ?><?php if($buttonStyle == "outline"): echo '<span>'; endif; ?><?php echo $buttonText ?><?php if($buttonStyle == "outline"): echo '</span>'; endif; ?></button>
+      </form>
+      <?php endif; ?>
     <?php endwhile; ?>
   </div>
   <?php endif; ?>
