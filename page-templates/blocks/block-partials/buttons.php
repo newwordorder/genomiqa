c2df38ea5a3aed99603e87c978d45c86f3b3100f
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-01-31 09:23:50 +1000

    updates

--- page-templates/blocks/block-partials/buttons.php
+++ page-templates/blocks/block-partials/buttons.php
@@ -1,21 +1,37 @@
-<?php if( get_sub_field('include_buttons') == 'yes' ): ?>
+<?php if (get_sub_field('include_buttons') == 'yes') : ?>
 
-  <?php if( have_rows('buttons') ): ?>
+  <?php if (have_rows('buttons')) : ?>
   <div class="buttons">
-    <?php while( have_rows('buttons') ): the_row();
-      $buttonText = get_sub_field('button_text');
-      $linkType = get_sub_field('link_type');
-      $url = get_sub_field('url');
-      $pageUrl = get_sub_field('pageurl');
-      $file = get_sub_field('file');
-      $buttonStyle = get_sub_field('button_style');
-      ?>
+    <?php while (have_rows('buttons')) : the_row();
+    $buttonText = get_sub_field('button_text');
+    $linkType = get_sub_field('link_type');
+    $url = get_sub_field('url');
+    $pageUrl = get_sub_field('pageurl');
+    $file = get_sub_field('file');
+    $buttonStyle = get_sub_field('button_style');
+    ?>
 
-      <?php if($linkType != "file"): ?>
-        <a href="<?php if($linkType == "page"): echo $pageUrl; endif; ?><?php if($linkType == "file"): echo $file; endif; ?><?php if($linkType == "url"):  echo $url; endif; ?>" class="btn btn--<?php echo $buttonStyle ?>" <?php if($linkType == "file"): echo 'download'; endif; ?>><?php if($buttonStyle == "outline"): echo '<span>'; endif; ?><?php echo $buttonText ?><?php if($buttonStyle == "outline"): echo '</span>'; endif; ?> <?php if($buttonStyle == "link"): echo '<i class="far fa-arrow-right"></i>'; endif; ?></a>
-      <?php else: ?> 
+      <?php if ($linkType !== "file") : ?>
+        <a href="<?php if ($linkType == "page") : echo $pageUrl;
+                endif; ?>
+                <?php if ($linkType == "url") : echo $url;
+                endif; ?>" class="btn btn--<?php echo $buttonStyle ?>">
+                <?php if ($buttonStyle == "outline") : echo '<span>';
+                endif; ?>
+                <?php echo $buttonText ?>
+                <?php if ($buttonStyle == "outline") : echo '</span>';
+                endif; ?> 
+                <?php if ($buttonStyle == "link") : echo '<i class="far fa-arrow-right"></i>';
+                endif; ?></a>
+      <?php else : ?> 
       <form method="get" action="<?php echo $file; ?>">
-        <button class="btn btn--<?php echo $buttonStyle ?>" type="submit"><?php if($buttonStyle == "outline"): echo '<span>'; endif; ?><?php echo $buttonText ?><?php if($buttonStyle == "outline"): echo '</span>'; endif; ?><?php if($buttonStyle == "link"): echo '<i class="far fa-arrow-right"></i>'; endif; ?></button>
+        <button class="btn btn--<?php echo $buttonStyle ?>" type="submit">
+        <?php if ($buttonStyle == "outline") : echo '<span>';
+        endif; ?><?php echo $buttonText ?>
+        <?php if ($buttonStyle == "outline") : echo '</span>';
+        endif; ?>
+        <?php if ($buttonStyle == "link") : echo '<i class="far fa-arrow-right"></i>';
+        endif; ?></button>
       </form>
       <?php endif; ?>
     <?php endwhile; ?>
